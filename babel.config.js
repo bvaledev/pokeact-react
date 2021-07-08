586938257c765dc35aee8329dbd803ddf2395a37
Brendo Vale <brendo.vale02@gmail.com>
2021-07-08 19:31:46 -0400

    init project with brt

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    presets: [
+        '@babel/preset-env',
+        '@babel/preset-typescript',
+        ['@babel/preset-react', { // para fazer o autoload do react sem importar ele em arquivos jsx
+			runtime: 'automatic'
+		}  ]
+    ]
+}
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import { render } from 'react-dom'
+
+function App(){
+    return <h1>Hello React</h1>
+}
+
+render(<App/>, document.getElementById('root'))
